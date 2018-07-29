dfe43744a123e405d27117d2bd664f3dd5bf5a58
yewenhe0904 <yewenhe0904@gmail.com>
2018-07-29 09:18:02 +0800

    add method Demo

---
+++ MethodDemo.java
@@ -0,0 +1,15 @@
+/*Method
+
+*/
+public class MethodDemo{
+	public static void main(String[] args){
+		System.out.println(calculateArea(5.5,3.4));
+	}
+	public static double calculateArea(double l,double w){
+		return l*w;
+	}
+	
+	public static double calculateArea(double l,double w){
+		return l*w;
+	}
+}
